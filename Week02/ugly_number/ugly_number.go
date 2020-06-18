4e8cb0dceba387c3296a319a4291dd3877f55f31
wangjihang <wangjihang@qutoutiao.net>
2020-06-18 21:52:29 +0800

    丑数

---
+++ Week02/ugly_number/ugly_number.go
@@ -0,0 +1,32 @@
+package ugly_number
+
+// 动态规划：
+// 时间复杂度：O(n)
+// 空间复杂度：O(n)
+// x[a]*2 > x[i] > x[a-1]*2
+func nthUglyNumber(n int) int {
+	a, b, c := 0, 0, 0
+	arr := make([]int, n)
+	arr[0] = 1
+	for i := 1; i < n; i++ {
+		na, nb, nc := arr[a]*2, arr[b]*3, arr[c]*5
+		arr[i] = min(na, min(nb, nc))
+		if arr[i] == na {
+			a++
+		}
+		if arr[i] == nb {
+			b++
+		}
+		if arr[i] == nc {
+			c++
+		}
+	}
+	return arr[n-1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
