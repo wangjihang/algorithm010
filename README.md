0b339126ed4db4bc8d966ea6f332d44417d61aa9
wangjihang <wangjihang@qutoutiao.net>
2020-06-23 19:38:04 +0800

    更新readme

--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 
 | 题目 | 第一二遍 | 第三遍 | 第四遍 | 第五遍 |
 |---|---|---|---|---|
-| [两数之和](https://leetcode-cn.com/problems/two-sum/) | 6.14 done | 6.15 done | 6.21 | 待定 |
-| [合并两个有序链表](https://leetcode-cn.com/problems/merge-two-sorted-lists/) | 6.15 done | 6.17 done | 6.22 | 待定 |
-| [删除排序数组中的重复项](https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/) | 6.15 done | 6.17 done | 6.22 | 待定 |
-| [设计循环双端队列](https://leetcode.com/problems/design-circular-deque/) | 6.15 done | 6.17 done | 6.22 | 待定 |
+| [两数之和](https://leetcode-cn.com/problems/two-sum/) | 6.14 done | 6.15 done | 6.21 done | 待定 |
+| [合并两个有序链表](https://leetcode-cn.com/problems/merge-two-sorted-lists/) | 6.15 done | 6.17 done | 6.22 done | 待定 |
+| [删除排序数组中的重复项](https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/) | 6.15 done | 6.17 done | 6.22 done | 待定 |
+| [设计循环双端队列](https://leetcode.com/problems/design-circular-deque/) | 6.15 done | 6.17 done | 6.22 done | 待定 |
 | [有效的字母异位词](https://leetcode-cn.com/problems/valid-anagram/description/) | 6.18 done | 6.19 done | 6.25 | 待定 |
 | [丑数](https://leetcode-cn.com/problems/chou-shu-lcof/) | 6.18 done | 6.19 done | 6.25 | 待定 |
 | [字母异位词分组](https://leetcode-cn.com/problems/group-anagrams/) | 6.18 done | 6.19 done | 6.25 | 待定 |
--- algorithm/algorithm.go
+++ algorithm/algorithm.go
@@ -1,19 +1,111 @@
 package algorithm
 
-func isAnagram(s string, t string) bool {
-	if len(s) != len(t) {
+type MyCircularDeque struct {
+	head     *DeNode
+	tail     *DeNode
+	length   int
+	capacity int
+}
+
+type DeNode struct {
+	value int
+	pre   *DeNode
+	next  *DeNode
+}
+
+/** Initialize your data structure here. Set the size of the deque to be k. */
+func Constructor(k int) MyCircularDeque {
+	head := &DeNode{}
+	tail := &DeNode{}
+	head.next = tail
+	tail.pre = head
+	return MyCircularDeque{
+		head:     head,
+		tail:     tail,
+		capacity: k,
+	}
+}
+
+/** Adds an item at the front of Deque. Return true if the operation is successful. */
+func (this *MyCircularDeque) InsertFront(value int) bool {
+	if this.IsFull() {
 		return false
 	}
-	arr := [26]int{}
-	length := len(s)
-	for i := 0; i < length; i++ {
-		arr[s[i]-'a']++
-		arr[t[i]-'a']--
+	node := &DeNode{
+		value: value,
+		pre:   this.head,
+		next:  this.head.next,
 	}
-	for _, v := range arr {
-		if v != 0 {
-			return false
-		}
-	}
+	this.head.next.pre = node
+	this.head.next = node
+	this.length++
 	return true
+}
+
+/** Adds an item at the rear of Deque. Return true if the operation is successful. */
+func (this *MyCircularDeque) InsertLast(value int) bool {
+	if this.IsFull() {
+		return false
+	}
+	node := &DeNode{
+		value: value,
+		pre:   this.tail.pre,
+		next:  this.tail,
+	}
+	this.tail.pre.next = node
+	this.tail.pre = node
+	this.length++
+	return true
+}
+
+/** Deletes an item from the front of Deque. Return true if the operation is successful. */
+func (this *MyCircularDeque) DeleteFront() bool {
+	if this.IsEmpty() {
+		return false
+	}
+	node := this.head.next
+	node.next.pre = this.head
+	this.head.next = node.next
+	node.pre, node.next = nil, nil
+	this.length--
+	return true
+}
+
+/** Deletes an item from the rear of Deque. Return true if the operation is successful. */
+func (this *MyCircularDeque) DeleteLast() bool {
+	if this.IsEmpty() {
+		return false
+	}
+	node := this.tail.pre
+	node.pre.next = this.tail
+	this.tail.pre = node.pre
+	node.pre, node.next = nil, nil
+	this.length--
+	return true
+}
+
+/** Get the front item from the deque. */
+func (this *MyCircularDeque) GetFront() int {
+	if this.IsEmpty() {
+		return -1
+	}
+	return this.head.next.value
+}
+
+/** Get the last item from the deque. */
+func (this *MyCircularDeque) GetRear() int {
+	if this.IsEmpty() {
+		return -1
+	}
+	return this.tail.pre.value
+}
+
+/** Checks whether the circular deque is empty or not. */
+func (this *MyCircularDeque) IsEmpty() bool {
+	return this.length == 0
+}
+
+/** Checks whether the circular deque is full or not. */
+func (this *MyCircularDeque) IsFull() bool {
+	return this.length == this.capacity
 }
