3900ca7386339f4c9399daae66846d8175a33731
wangjihang <wangjihang@qutoutiao.net>
2020-07-06 14:18:17 +0800

    readme

--- algorithm/algorithm.go
+++ algorithm/algorithm.go
@@ -1,36 +1,26 @@
 package algorithm
 
-func permuteUnique(nums []int) [][]int {
-	var (
-		length    = len(nums)
-		result    = make([][]int, 0)
-		backtrack func(path []int, used []bool)
-	)
-	backtrack = func(path []int, used []bool) {
-		if len(path) == length {
-			result = append(result, Copy(path, length))
-			return
-		}
-		usedM := make(map[int]bool) // map[value]bool
-		for i, v := range nums {
-			if !used[i] && !usedM[v] {
-				used[i] = true
-				usedM[v] = true
-				path = append(path, v)
-
-				backtrack(path, used)
-
-				used[i] = false
-				path = path[:len(path)-1]
+func lemonadeChange(bills []int) bool {
+	var five, ten int
+	for _, bill := range bills {
+		if bill == 5 {
+			five++
+		} else if bill == 10 {
+			if five < 1 {
+				return false
+			}
+			five--
+			ten++
+		} else {
+			if ten > 0 && five > 0 {
+				ten--
+				five--
+			} else if five > 3 {
+				five -= 3
+			} else {
+				return false
 			}
 		}
 	}
-	backtrack(make([]int, 0, length), make([]bool, length))
-	return result
-}
-
-func Copy(track []int, length int) []int {
-	dup := make([]int, length)
-	copy(dup, track)
-	return dup
+	return true
 }
